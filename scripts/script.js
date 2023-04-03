727b98934a7688518e365411e00fff2bd605c5d1
JustANipple <prandisamuel99@gmail.com>
2023-04-03 16:26:15 +0200

    modified:   index.html 	modified:   scripts/script.js 	modified:   styles/css/main.css 	modified:   styles/css/main.css.map 	modified:   styles/scss/components/_form.scss

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+const nameRegex = new RegExp(/^[a-zA-Z\s]+$/);
+const numberRegex = new RegExp(/^\d{16}$/);
+const monthRegex = new RegExp(/^(0?[1-9]|1[0-2])$/);
+const yearRegex = new RegExp(/^(19|20)?\d{2}$/);
+const cvcRegex = new RegExp(/^\d{3,4}$/);
+const regexArr = [nameRegex, numberRegex, monthRegex, yearRegex, cvcRegex];
+const inputsArr = document.querySelectorAll("input");
+const form = document.querySelector(".form");
+const success = document.querySelector(".success");
+
+const sendForm = document.querySelector(".card_data_submit");
+
+sendForm.addEventListener("click", checkForm);
+
+function checkForm() {
+let rights = 0;
+
+    for(let i = 0; i < regexArr.length; i++) {
+        if(!regexArr[i].test(inputsArr[i].value)) {
+            inputsArr[i].style.borderColor = "red";
+            inputsArr[i].parentNode.querySelector("p").style.display = "block";
+        } else {
+            inputsArr[i].style.borderColor = "purple";
+            inputsArr[i].parentNode.querySelector("p").style.display = "none";
+            rights++;
+        }
+    }
+
+    if(rights === 5) {
+        
+    }
+}
